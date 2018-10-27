a3c2bf7c53eca9378e07c057a594b05d0a9c9292
bordoisila1 <Siku1234>
2018-10-27 16:10:35 -0400

    some major updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import HomePage from './components/HomePage';
-import NavBar from './components/HeaderComponent/NavBar';
-import Footer from './components/FooterComponent/Footer';
+import { BrowserRouter as Router } from "react-router-dom";
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-        <div>
-            <NavBar/>
-            <HomePage/>
-            <Footer/>
-        </div>
+        <Router>
+            <div>
+                <HomePage/>
+            </div>
+        </Router>
     );
   }
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,114 @@
-import React, { Component } from 'react';
-class HomePage extends Component {
-    render() {
-        return (
-            <div>
-                Home page
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const HomePage = () => (
+    <Router>
+        <div className="parent_container">
+            <div className="nav_bar">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/interests">Interests</Link>
+                    </li>
+                </ul>
             </div>
-        )
-    }
-}
+
+            <hr />
+            <div className="lower_container">
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/interests" component={Interests} />
+            </div>
+        </div>
+    </Router>
+);
+
+const Home = () => (
+    <div>
+        <h2>Welcome to my Website</h2>
+        {/*<img className="intro_image" src="/images/banana_large.jpg"/>
+        <div className=""><p>I am Santanu Baruah,
+            a technology professional working for a Bank in the New York city.
+            My hobbies are making Chai and Chicken curry.
+            When I am bored, I go for long walks.</p>
+        </div>*/}
+        <div className="container">
+            <div className="row">
+                <div className="col about_grid_item rounded">
+                    <h6 className="intro_text_container">I am Santanu Baruah, a technology professional in a bank. When I am not working there, I am busy walking<i class="em em-man-walking"></i> or making Chai.<i class="em em-coffee"></i></h6>
+                    <a href="https://www.youtube.com/watch?v=6Tou8-Cz8is" target="_blank"><img className="grid_image_big rounded" src="/images/banana_large.jpg"/></a>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>About me</h2>
+        <div className="container">
+            <div className="row">
+                <div className="col about_grid_item rounded">
+                    <h5>Linkedin</h5>
+                    <a href="https://www.linkedin.com/in/santanu-baruah-gs/" target="_blank"><img className="grid_image rounded" src="/images/linkedin_logo.jpg"/></a>
+                </div>
+                <div className="col about_grid_item rounded">
+                    <h5>GitHub</h5>
+                    <a href="https://github.com/bordoisila1" target="_blank"><img className="grid_image rounded" src="/images/github_logo.jpg"/></a>
+                </div>
+                <div className="w-100"></div>
+                <div className="col about_grid_item rounded">
+                    <h5>Currently listening to..</h5>
+                    <a href="https://www.youtube.com/channel/UCqNxhPZoLJ81i5QaK4nqn8A" target="_blank"><img className="grid_image rounded" src="/images/cas_logo.jpg"/></a>
+                </div>
+                <div className="col about_grid_item rounded">
+                    <h5>Currently Reading...</h5>
+                    <a href="https://www.amazon.com/Silk-Roads-New-History-World/dp/1101912375" target="_blank"><img className="grid_image rounded" src="/images/silkroads_book_logo.jpg"/></a>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+const Interests = ({ match }) =>
+    (
+        <div>
+            <h2>Topics</h2>
+            <ul className="nav_bar">
+                <li>
+                    <a href="https://www.citibikenyc.com/">
+                        <img src="https://d21xlh2maitm24.cloudfront.net/nyc/01unlock2.JPG?mtime=20160428123800"/>
+                    </a>
+                </li>
+                <li path={match.path}>
+                    <Link to={`${match.url}/history`}>History</Link>
+                </li>
+                <li path={match.path}>
+                    <Link to={`${match.url}/chai`}>Chai</Link>
+                </li>
+                <li path={match.path}>
+                    <Link to={`${match.url}/books`}>Books</Link>
+                </li>
+            </ul>
+
+            <Route path={`${match.path}/:interestId`} component={Interest}/>
+            <Route
+                exact
+                path={match.path}
+                render={() => <h3>Please select a topic.</h3>}
+            />
+        </div>
+    );
+
+const Interest = ({ match }) => (
+    <div>
+        <h3>{match.params.interestId}</h3>
+    </div>
+);
+
 export default HomePage;
